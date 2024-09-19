7f5b8ec1f3400bc8655e0c4159174b5ef1289059
Tarik <tarik.jaidani@gmail.com>
2024-09-19 17:20:09 +0200

    First commit

---
+++ script.js
@@ -0,0 +1,13 @@
+rdvAmoureux  = [
+    "Quelle est ta destination de rêve pour des vacances et pourquoi ?",
+    "Quel est le meilleur livre que tu aies jamais lu ?",
+    "Si tu pouvais dîner avec n'importe quelle personne, vivante ou décédée, qui choisirais-tu ?",
+    "Quelle est ta passion secrète ou ton hobby préféré ?",
+    "Quel est le film qui t'a le plus marqué et pourquoi ?",
+    "Si tu pouvais apprendre une nouvelle compétence instantanément, laquelle choisirais-tu ?",
+    "Quelle est ta plus belle mémoire d'enfance ?",
+    "Quel est le plat que tu pourrais manger tous les jours sans te lasser ?",
+    "Si tu pouvais vivre dans n'importe quelle époque, laquelle choisirais-tu et pourquoi ?",
+    "Quel est le meilleur conseil que tu aies jamais reçu ?"
+  ]
+  
